1a67aa7f275599d709d22a56aff7efbe3db54eeb
Maxim Filippov <mr.maks.filippov.94@mail.ru>
2026-05-26 19:08:33 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/EventRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/EventRowMapper.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.model.Event;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 
 @Component
 public class EventRowMapper implements RowMapper<Event> {
