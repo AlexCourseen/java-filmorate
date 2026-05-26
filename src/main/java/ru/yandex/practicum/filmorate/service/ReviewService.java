0005019a194169f7775c266daaef08ce51c5f33f
marianik <nik1mari@yandex.ru>
2026-05-26 14:15:56 -0700

    правки после ревью

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -29,17 +29,17 @@ public class ReviewService {
     private final FilmStorage filmStorage;
 
     public Review createReview(Review review) {
-        validateUserExists(review.getUserId());
-        validateFilmExists(review.getFilmId());
+        userStorage.getUser(review.getUserId());
+        filmStorage.getFilm(review.getFilmId());
 
         if (reviewStorage.existsByUserAndFilm(review.getUserId(), review.getFilmId())) {
             throw new ValidationException("Пользователь уже оставил отзыв на этот фильм");
         }
 
-        Review created = reviewStorage.create(review);
+        Review createdReview = reviewStorage.create(review);
         log.info("Создан отзыв с id={} для фильма {} пользователем {}",
-                created.getReviewId(), created.getFilmId(), created.getUserId());
-        return created;
+                createdReview.getReviewId(), createdReview.getFilmId(), createdReview.getUserId());
+        return createdReview;
     }
 
     public Review updateReview(Review review) {
@@ -51,13 +51,13 @@ public class ReviewService {
         existing.setContent(review.getContent());
         existing.setIsPositive(review.getIsPositive());
 
-        Review updated = reviewStorage.update(existing);
-        log.info("Обновлён отзыв с id={}", updated.getReviewId());
-        return updated;
+        Review updatedReview = reviewStorage.update(existing);
+        log.info("Обновлён отзыв с id={}", updatedReview.getReviewId());
+        return updatedReview;
     }
 
     public void deleteReview(Long reviewId) {
-        Review review = getReviewById(reviewId);
+        getReviewById(reviewId);
         reviewLikeStorage.removeAllByReviewId(reviewId);
         reviewStorage.delete(reviewId);
         log.info("Удалён отзыв с id={}", reviewId);
@@ -69,18 +69,19 @@ public class ReviewService {
     }
 
     public Collection<Review> getReviews(Long filmId, Integer count) {
-        int limit = (count == null || count <= 0) ? 10 : count;
-
+        if (count <= 0) {
+            throw new ValidationException("Число отзывов должно быть положительным");
+        }
         if (filmId != null) {
-            validateFilmExists(filmId);
-            return reviewStorage.getByFilmId(filmId, limit);
+            filmStorage.getFilm(filmId);
+            return reviewStorage.getByFilmId(filmId, count);
         } else {
-            return reviewStorage.getAll(limit);
+            return reviewStorage.getAll(count);
         }
     }
 
     public void addDislike(Long reviewId, Long userId) {
-        validateUserExists(userId);
+        userStorage.getUser(userId);
         Review review = getReviewById(reviewId);
 
         if (reviewLikeStorage.existsLike(reviewId, userId)) {
@@ -98,7 +99,7 @@ public class ReviewService {
     }
 
     public void addLike(Long reviewId, Long userId) {
-        validateUserExists(userId);
+        userStorage.getUser(userId);
         Review review = getReviewById(reviewId);
 
         if (reviewLikeStorage.existsDislike(reviewId, userId)) {
@@ -132,22 +133,6 @@ public class ReviewService {
             int newUseful = review.getUseful() + 1;
             reviewStorage.updateUseful(reviewId, newUseful);
             log.info("Пользователь {} удалил дизлайк с отзыва {}", userId, reviewId);
-        }
-    }
-
-    private void validateUserExists(Long userId) {
-        try {
-            userStorage.getUser(userId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователь с id=" + userId + " не найден");
-        }
-    }
-
-    private void validateFilmExists(Long filmId) {
-        try {
-            filmStorage.getFilm(filmId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Фильм с id=" + filmId + " не найден");
         }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewLikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewLikeDbStorage.java
@@ -1,14 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.ReviewLike;
+import ru.yandex.practicum.filmorate.storage.BaseDbStorage;
+import ru.yandex.practicum.filmorate.storage.mappers.ReviewLikeRowMapper;
 
 @Repository
-@RequiredArgsConstructor
-public class ReviewLikeDbStorage implements ReviewLikeStorage {
-
-    private final JdbcTemplate jdbcTemplate;
+public class ReviewLikeDbStorage extends BaseDbStorage<ReviewLike> implements ReviewLikeStorage {
 
     private static final String ADD_LIKE = """
         INSERT INTO review_likes (review_id, user_id, is_like)
@@ -40,40 +39,44 @@ public class ReviewLikeDbStorage implements ReviewLikeStorage {
         DELETE FROM review_likes WHERE review_id = ?
     """;
 
+    public ReviewLikeDbStorage(JdbcTemplate jdbc, ReviewLikeRowMapper mapper) {
+        super(jdbc, mapper);
+    }
+
     @Override
     public void addLike(Long reviewId, Long userId) {
-        jdbcTemplate.update(ADD_LIKE, reviewId, userId);
+        update(ADD_LIKE, reviewId, userId);
     }
 
     @Override
     public void addDislike(Long reviewId, Long userId) {
-        jdbcTemplate.update(ADD_DISLIKE, reviewId, userId);
+        update(ADD_DISLIKE, reviewId, userId);
     }
 
     @Override
     public void removeLike(Long reviewId, Long userId) {
-        jdbcTemplate.update(REMOVE_LIKE, reviewId, userId);
+        update(REMOVE_LIKE, reviewId, userId);
     }
 
     @Override
     public void removeDislike(Long reviewId, Long userId) {
-        jdbcTemplate.update(REMOVE_DISLIKE, reviewId, userId);
+        update(REMOVE_DISLIKE, reviewId, userId);
     }
 
     @Override
     public boolean existsLike(Long reviewId, Long userId) {
-        Integer count = jdbcTemplate.queryForObject(EXISTS_LIKE, Integer.class, reviewId, userId);
+        Integer count = jdbc.queryForObject(EXISTS_LIKE, Integer.class, reviewId, userId);
         return count != null && count > 0;
     }
 
     @Override
     public boolean existsDislike(Long reviewId, Long userId) {
-        Integer count = jdbcTemplate.queryForObject(EXISTS_DISLIKE, Integer.class, reviewId, userId);
+        Integer count = jdbc.queryForObject(EXISTS_DISLIKE, Integer.class, reviewId, userId);
         return count != null && count > 0;
     }
 
     @Override
     public void removeAllByReviewId(Long reviewId) {
-        jdbcTemplate.update(REMOVE_ALL_BY_REVIEW_ID, reviewId);
+        update(REMOVE_ALL_BY_REVIEW_ID, reviewId);
     }
 }
